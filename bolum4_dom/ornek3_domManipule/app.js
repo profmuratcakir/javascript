072b0de3d0694d783ad8e64af69293a6ff9ca3d9
profmuratcakir <mcakiroglu.techproed@gmail.com>
2021-05-28 23:58:17 +0300

    day10

--- bolum4_dom/ornek3_domManipule/app.js
+++ bolum4_dom/ornek3_domManipule/app.js
@@ -35,23 +35,41 @@ h1.appendChild(yazi);
 sonDiv.after(h1); //input-div'in sonrasina h1'i ekliyoruz.
 h1.className = "yeni-h1"; // olustrdugumuz h1 elemanına bir class adı atadık.
 
+// Ekle butonuna  her tıklandiginda
 document.querySelector(".ekle").onclick = function () {
   // ekleme yapılacak listeyi al
   const liste = document.querySelector(".liste");
   //input elemanina girilen yeni satirin degerini al
-  const satir = document.querySelector(".dil").value;
+  const satir = document.querySelector(".dil");
   // yeni girilen satiri saklamak icin bir li olusturduk.
   const yeniLi = document.createElement("li");
   // yeni li icin textnode olusturduk
-  const textNode = document.createTextNode(satir);
+  const textNode = document.createTextNode(satir.value);
   //olusturdugumuz texnode'u yeni li'ye bagladik.
   yeniLi.appendChild(textNode);
   // yeni eklenen satiri var olan listeye (ul) baglayalim.
   liste.appendChild(yeniLi);
+  //input içinde kalan veriyi sil
+  satir.value = "";
 };
 
+// Sil butonuna  her tıklandiginda
 document.querySelector(".sil").onclick = function () {
   const liste = document.querySelector(".liste");
   // liste.removeChild(liste.firstElementChild);  // ilk elemani sil
   liste.removeChild(liste.lastElementChild); // son elemani sil
+};
+
+// Klavyeden bir tusa basildiginda calis
+document.querySelector(".dil").onkeydown = function (event) {
+  // Eger basilan tus Enter tusu ise
+  if (event.keyCode === 13) {
+    // Ekle butonunun tiklama fonksiyonunu cagir.
+    document.querySelector(".ekle").click();
+  }
+  // del tuşuna basılırsa
+  if (event.keyCode === 46) {
+    // Sil butonunun tiklama fonksiyonunu cagir.
+    document.querySelector(".sil").click();
+  }
 };
--- bolum5_diziler/ornek3_iterasyon/iterasyon.js
+++ bolum5_diziler/ornek3_iterasyon/iterasyon.js
@@ -1,16 +1,204 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                        DİZİLERDE İTERASYON
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ======================================================
+//                     FOR DÖNGÜSÜ
+// ======================================================
+
 const koordinatlar = [120, 100, -100, 220, 330];
 let negatif = 0;
 let pozitif = 0;
 
-// for (let i = 0; i < koordinatlar.length; i++) {
-//   koordinatlar[i] < 0 ? negatif++ : pozitif++;
-// }
-
-// FOR IN DONGUSU
-for (let i in koordinatlar) {
+for (let i = 0; i < koordinatlar.length; i++) {
   koordinatlar[i] < 0 ? negatif++ : pozitif++;
 }
 
 console.log(
   `Negatif Koordinat Sayısı: ${negatif} Pozitif Koordinat sayisi ${pozitif}`
 );
+
+// ======================================================
+//                   FOR IN DÖNGÜSÜ
+// ======================================================
+
+//-------------------------------------------------------
+// SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+// bir dizide saklandığı varsayalım.Bu hayvanları türüne
+// göre aramamızı sağlayacak ve o türden kaç adet bulunduğu
+//  ana programa döndürecek fonksiyonu yazınız. Eğer o
+// türden bir hayvan yok ise bulunamadığını yazdırsın.
+//--------------------------------------------------------
+
+const hayvanlar = ["fil", "deve", "kuş", "deve", "fare", "kedi", "deve"];
+const ara = prompt("aramak istenilen hayvan türünü giriniz:").toLowerCase();
+
+const bulDondur = function () {
+  let hayvanSayisi = 0;
+  for (let i in hayvanlar) {
+    if (hayvanlar[i] == ara) {
+      hayvanSayisi++;
+    }
+  }
+  return hayvanSayisi;
+};
+const sayi = bulDondur(ara);
+sayi == 0
+  ? console.log("Aradiginiz hayvan bulunamadi")
+  : console.log(`Aradiginiz ${ara} 'den ${sayi} adet bulunmaktadir`);
+
+// ======================================================
+//                   FOR OF DÖNGÜSÜ
+// ======================================================
+
+// for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. Bu dongude dizi icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+let arabalar = ["BMW", "Volvo", "Mini"];
+let yazı = "";
+for (let i of arabalar) {
+  console.log(i);
+  yazı += i + " "; // i:  dizinin i. elemani demektir.
+}
+console.log(yazı);
+
+// ======================================================
+//                   FOREACH METODU
+// ======================================================
+
+//-------------- ÖRNEK -------------------
+let ogrenciler = ["John", "Ali", "Can"];
+
+function yazdır(a) {
+  console.log(a);
+}
+
+ogrenciler.forEach(yazdır);
+ogrenciler.forEach((x) => console.log(x));
+
+//-------------- ÖRNEK -------------------
+const fiyatlar = [200, 300, 125, 450, 333, 123];
+let toplam = 0;
+fiyatlar.forEach((f) => (toplam += f));
+console.log(toplam);
+document.querySelector(".toplam").innerHTML = `${toplam} TL`;
+
+//-------------- ÖRNEK -------------------
+// forEach ile Parametre kullanım detaylari
+toplam = 0;
+fiyatlar.forEach((a, i, d) => {
+  toplam += a;
+  console.log(` ${i} . iterasyon: ${toplam}`);
+});
+
+console.log(toplam);
+document.querySelector(".toplam").innerHTML = `${toplam} TL`;
+
+//-------------- ÖRNEK -------------------
+// fiyatlar dizisinde her bir fiyata %10 zam yapalım.
+let toplamFiyat = 0;
+let zamliFiyatlar = [];
+fiyatlar.forEach((deger, indis) => {
+  zamliFiyatlar[indis] = (deger * 1.1).toFixed(2);
+  toplamFiyat += Number(zamliFiyatlar[indis]);
+});
+console.log(zamliFiyatlar);
+console.log(toplamFiyat);
+
+// ======================================================
+//                       MAP METODU
+// ======================================================
+
+//-------------- ÖRNEK -------------------
+// Örnek : Dizinin herbir elemaninin 2 katını aliniz.
+
+const rakamlar = [3, 5, 3, 2, 6, 7, 9];
+const ikiKat = rakamlar.map((x) => x * 2);
+console.log(ikiKat, rakamlar);
+
+//-------------- ÖRNEK -------------------
+// isimler dizisin içerisindeki her ismi büyük harf olarak
+// yeni bir diziye saklayalım.
+const isimler = [
+  "Ahmet",
+  "mehmet",
+  "ismet",
+  "SAFFET",
+  "Can",
+  "Canan",
+  "Cavidan",
+];
+
+// map() metodu normal şartlarda bir dizi döndürür.
+const buyuk = isimler.map((isim) => isim.toUpperCase());
+buyuk.forEach((isim) => console.log(isim));
+
+// map() metodundan sonra eğer forEach gibi bir terminal işlemi
+// kullanılırsa map() metodu bir dizi döndürmez.
+isimler.map((isim) => isim.toUpperCase()).forEach((isim) => console.log(isim));
+
+//-------------- ÖRNEK -------------------
+// tlFiyatlar dizisindeki ürün fiyatlarini HTLM inputlarina girilen
+// Dolar ve Euro paritelerine göre dolar ve euroya çevirerek HTML de
+// gösteriniz.
+tlFiyatlar = [120, 340, 550, 245, 322.5, 789];
+
+// Kur inputlarini al.
+const dolarKur = document.querySelector(".dolar");
+const yuroKur = document.querySelector(".yuro");
+
+//dolar ve euro fiyatlari yazacak elemanlari al
+const dolar = document.querySelector(".dolar-fiyat");
+const yuro = document.querySelector(".yuro-fiyat");
+
+// dolar kur input'unun değeri degistiginde urunlerin
+// dolar fiyatlarini güncelle.
+dolarKur.onchange = function () {
+  dolarKur.value < 0
+    ? alert("Kur 0'dan küçük olamaz")
+    : (dolar.innerHTML = tlFiyatlar.map((tl) =>
+        Math.round(tl / dolarKur.value)
+      ));
+};
+
+// euro kur input'unun değeri degistiginde urunlerin
+// euro fiyatlarini güncelle.
+yuroKur.onchange = function () {
+  yuroKur.value < 0
+    ? alert("Kur 0'dan küçük olamaz")
+    : (yuro.innerHTML = tlFiyatlar.map((tl) => Math.round(tl / yuroKur.value)));
+};
+
+//-------------- ÖRNEK -------------------
+// tlFiyatlar dizisindeki ürün fiyatlarinin 250 TL altında olanlarına
+// %10 zam, üstünde olanlarına da %20 zam yapılmak isteniyor. Ayrıca
+// zamli fiyatlar aşağıdaki formatta saklanmak isteniyor.
+// 1. ürünün zamlı fiyati 110 TL gibi
+
+const zamliTlFiyatlar = tlFiyatlar.map((deger, indis) => {
+  if (deger < 250) {
+    return `${indis + 1}.ürünün zamli fiyati : ${deger * 1.1}`;
+  } else {
+    return `${indis + 1}.ürünün zamli fiyati : ${deger * 1.2}
+    `;
+  }
+});
+
+document.querySelector(".zamli-fiyat").innerHTML = zamliTlFiyatlar;
+console.log(zamliTlFiyatlar);
+
+// ======================================================
+//                       FILTER METODU
+// ======================================================
+// tlFiyatlar listesinde fiyatı 250 TL den az olanlari ayri
+// bir diziye saklayalim.
+
+const kucuk250 = tlFiyatlar.filter((d) => d < 250);
+console.log(kucuk250);
+
+//fiyati 350 küçük olanlari yazdiriniz.
+tlFiyatlar.filter((d) => d < 350).forEach((x) => console.log(x));
+
+// Küçükten büyüğe sıralama
+console.log(tlFiyatlar.sort((a, b) => a - b));
+
+// Büyükten küçüğe sıralama
+console.log(tlFiyatlar.sort((a, b) => b - a));
